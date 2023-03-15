1f70ad2340d6f6b550eb142c83fdf159100728fc
KCVenus <arthur.toulon@gmail.com>
2023-03-15 11:08:26 +0100

    ajout 2 iem test
    test passe bien à faux + ajout constantes

--- Html_PiloterParTest_Css_Js/public_html/js/javascript.js
+++ Html_PiloterParTest_Css_Js/public_html/js/javascript.js
@@ -6,6 +6,15 @@ function GetTarrif(poids){
     
     
     const Poids250g = 250;
+    const Poids500g = 250;
+    const Poids750g = 250;
+    const Poids1kg = 250;
+    const Poids2kg = 250;
+    const Poids5kg = 250;
+    const Poids10kg = 250;
+    const Poids15kg = 250;
+    const Poids30kg = 250;
+
     
     if(Poids250g == poids)
     {
--- Html_PiloterParTest_Css_Js/public_html/unit/test/testUnit.js
+++ Html_PiloterParTest_Css_Js/public_html/unit/test/testUnit.js
@@ -4,38 +4,38 @@ MesTestsUnitaires.prototype.test = function () {
     assertEquals('test de fonctionnement', 1, premierTest());
 };
 
-MesTestsUnitaires.prototype.testpoid250 = function (){
+MesTestsUnitaires.prototype.testPoids250 = function (){
     assertEquals('test pour 250g',250, GetTarrif(250));
 };
 
+MesTestsUnitaires.prototype.testpoids500 = function (){
+    assertEquals('test pour 500g',500, GetTarrif(500));
+};
+// 
 // MesTestsUnitaires.prototype.test = function (){
-//     assertEquals('test pour 500g',250, GetTarrif(250));
+//     assertEquals('test pour 750g',750, GetTarrif(750));
 // };
 // 
 // MesTestsUnitaires.prototype.test = function (){
-//     assertEquals('test pour 750g',250, GetTarrif(250));
+//     assertEquals('test pour 1kg',1000, GetTarrif(1000));
 // };
 // 
 // MesTestsUnitaires.prototype.test = function (){
-//     assertEquals('test pour 1kg',250, GetTarrif(250));
+//     assertEquals('test pour 2kg',2000, GetTarrif(2000));
 // };
 // 
 // MesTestsUnitaires.prototype.test = function (){
-//     assertEquals('test pour 2kg',250, GetTarrif(250));
+//     assertEquals('test pour 5kg',5000, GetTarrif(5000));
 // };
 // 
 // MesTestsUnitaires.prototype.test = function (){
-//     assertEquals('test pour 5kg',250, GetTarrif(250));
+//     assertEquals('test pour 10kg',10000, GetTarrif(10000));
 // };
 // 
 // MesTestsUnitaires.prototype.test = function (){
-//     assertEquals('test pour 10kg',250, GetTarrif(250));
-// };
-// 
-// MesTestsUnitaires.prototype.test = function (){
-//     assertEquals('test pour 15kg',250, GetTarrif(250));
+//     assertEquals('test pour 15kg',15000, GetTarrif(15000));
 // };
 
 // MesTestsUnitaires.prototype.test = function (){
-// assertEquals('test pour 30kg',250, GetTarrif(250));
+// assertEquals('test pour 30kg',30000, GetTarrif(30000));
 // };
