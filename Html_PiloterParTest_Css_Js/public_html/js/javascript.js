06d675409483ce96242da26deb7cc2b885bf354c
KCVenus <arthur.toulon@gmail.com>
2023-03-15 12:11:36 +0100

    refactorisation de la fonction
    test ok

--- Html_PiloterParTest_Css_Js/public_html/js/javascript.js
+++ Html_PiloterParTest_Css_Js/public_html/js/javascript.js
@@ -1,6 +1,61 @@
  function premierTest(){
     return 1;
 }
+//
+//function GetTarrif(poids){
+//    
+//    
+//    const Poids250g = 250;
+//    const Poids500g = 500;
+//    const Poids750g = 750;
+//    const Poids1kg = 1000;
+//    const Poids2kg = 2000;
+//    const Poids5kg = 5000;
+//    const Poids10kg = 10000;
+//    const Poids15kg = 15000;
+//    const Poids30kg = 30000;
+//
+//    
+//    if(poids === Poids250g)
+//    {
+//        return 250;
+//    }
+//    
+//    if (poids === Poids500g)
+//    {
+//        return 500;
+//    }
+//    
+//    if (poids === Poids750g)
+//    {
+//        return 750;
+//    }
+//    
+//    if (poids === Poids1kg)
+//    {
+//        return 1000;
+//    }
+//    if (poids === Poids2kg)
+//    {
+//        return 2000;
+//    }
+//    if (poids === Poids5kg)
+//    {
+//        return 5000;
+//    }
+//    if (poids === Poids10kg)
+//    {
+//        return 10000;
+//    }
+//    if (poids === Poids15kg)
+//    {
+//        return 15000;
+//    }
+//    if (poids === Poids30kg)
+//    {
+//        return 30000;
+//    }
+//}
 
 function GetTarrif(poids){
     
@@ -14,45 +69,34 @@ function GetTarrif(poids){
     const Poids10kg = 10000;
     const Poids15kg = 15000;
     const Poids30kg = 30000;
+    somme =0;
 
-    
-    if(poids === Poids250g)
-    {
-        return 250;
-    }
-    
-    if (poids === Poids500g)
-    {
-        return 500;
-    }
-    
-    if (poids === Poids750g)
-    {
-        return 750;
-    }
-    
-    if (poids === Poids1kg)
-    {
-        return 1000;
-    }
-    if (poids === Poids2kg)
-    {
-        return 2000;
-    }
-    if (poids === Poids5kg)
-    {
-        return 5000;
-    }
-    if (poids === Poids10kg)
-    {
-        return 10000;
-    }
-    if (poids === Poids15kg)
-    {
-        return 15000;
-    }
-    if (poids === Poids30kg)
-    {
-        return 30000;
-    }
+
+ if (poids > 0 & poids <= Poids250g){
+     return somme = 4.95; // prix pour 250g
+     }
+ if (poids >Poids250g & poids <=Poids500g){
+    return somme = 6.70;   // prix pour 500g
 }
+if (poids > Poids500g & poids <= Poids750g){
+   return somme = 7.60;   // prix pour 750g
+}
+if (poids > Poids750g & poids <=Poids1kg ) {
+    return somme = 8.25;   // prix pour 1kg
+ }
+ if (poids >Poids1kg & poids <=Poids2kg ) {
+   return somme =9.55;    // prix pour 2kg
+} 
+if ( poids > Poids2kg & poids <= Poids5kg){
+   return somme = 14.65;  // prix pour 5kg
+}
+if (poids >Poids5kg & poids <=Poids10kg ){
+    return somme = 21.30;  // prix pour 10kg
+} 
+if (poids >Poids10kg & poids <= Poids15kg){
+    return somme = 26.95;  // prix pour 15kg
+} 
+if (poids > Poids15kg & poids <=Poids30kg){
+    return somme = 33.40;  // prix pour 30kg
+ }
+ }
--- Html_PiloterParTest_Css_Js/public_html/unit/test/testUnit.js
+++ Html_PiloterParTest_Css_Js/public_html/unit/test/testUnit.js
@@ -5,37 +5,37 @@ MesTestsUnitaires.prototype.test = function () {
 };
 
 MesTestsUnitaires.prototype.testPoids250 = function (){
-    assertEquals('test pour 250g',250, GetTarrif(250));
+    assertEquals('test pour 250g',4.95, GetTarrif(250));
 };
 
 MesTestsUnitaires.prototype.testPoids500 = function (){
-    assertEquals('test pour 500g',500, GetTarrif(500));
+    assertEquals('test pour 500g',6.70, GetTarrif(500));
 };
  
  MesTestsUnitaires.prototype.testPoids750 = function (){
-    assertEquals('test pour 750g',750, GetTarrif(750));
+    assertEquals('test pour 750g',7.60, GetTarrif(750));
  };
 
  MesTestsUnitaires.prototype.testPoids1000 = function (){
-    assertEquals('test pour 1kg',1000, GetTarrif(1000));
+    assertEquals('test pour 1kg',8.25, GetTarrif(1000));
 };
 
  MesTestsUnitaires.prototype.testPoids2000 = function (){
-    assertEquals('test pour 2kg',2000, GetTarrif(2000));
+    assertEquals('test pour 2kg',9.55, GetTarrif(2000));
  };
  
  MesTestsUnitaires.prototype.testPoids5000 = function (){
-     assertEquals('test pour 5kg',5000, GetTarrif(5000));
+     assertEquals('test pour 5kg',14.65, GetTarrif(5000));
  };
  
  MesTestsUnitaires.prototype.testPoids10000 = function (){
-     assertEquals('test pour 10kg',10000, GetTarrif(10000));
+     assertEquals('test pour 10kg',21.30, GetTarrif(10000));
  };
  
  MesTestsUnitaires.prototype.testPoids15000 = function (){
-    assertEquals('test pour 15kg',15000, GetTarrif(15000));
+    assertEquals('test pour 15kg',26.95, GetTarrif(15000));
  };
 
  MesTestsUnitaires.prototype.testPoids30000 = function (){
- assertEquals('test pour 30kg',30000, GetTarrif(30000));
+ assertEquals('test pour 30kg',33.40, GetTarrif(30000));
  };
